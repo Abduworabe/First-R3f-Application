9edaa64918d62660b731dd581a6f8cf525cee882
Abduworabe <abdilkerimjuhar@gmail.com>
2025-04-22 05:05:31 -0800

    ✨ feat: add rotating group of objects with box and sphere
    🔄 Implemented a group to animate a box and a sphere together.
    🎮 Added OrbitControls for enhanced camera interaction.
    🌍 Created a green plane as the ground.

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,21 @@ import { OrbitControls } from '@react-three/drei';
 
 function App() {
   const boxRef = useRef();
+  const groupRef=useRef();
   useFrame((state, delta) => {
-    //we need to know hoe much time has passed sone the last frame
+    //1, we need to know hoe much time has passed sone the last frame
     //means delta time
     boxRef.current.rotation.y += delta;
+    groupRef.current.rotation.y+=delta;
   });
+  //2, like in Three.js, we can group objects with the Group class
+  // Then are goinge to group th ecube and the sphere(not the floor) and them animate the group like a carousel
+
+
   return (
     <>
       <OrbitControls />
+      <group ref={groupRef}>
       <mesh position-x={1.5} rotation-y={Math.PI * 0.25} scale={1} ref={boxRef}>
         {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
         <boxGeometry scale={1.5} />
@@ -22,6 +29,9 @@ function App() {
         <sphereGeometry />
         <meshBasicMaterial color={'orange'} />
       </mesh>
+
+      </group>
+     
       <mesh scale={7} rotation-x={-Math.PI * 0.5} position-y={-1}>
         <planeGeometry />
         <meshBasicMaterial color={'green'} />
