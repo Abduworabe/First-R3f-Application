059f7d2c2d2c8bc48084f475ecba989ac2d3d065
Abduworabe <abdilkerimjuhar@gmail.com>
2025-04-22 05:26:33 -0800

    ✨ feat: enhance 3D scene with lighting and materials
    🔄 Added ambient and directional lights for improved scene illumination.
    🎨 Updated materials to use meshStandardMaterial for better rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,41 @@ import { OrbitControls } from '@react-three/drei';
 
 function App() {
   const boxRef = useRef();
-  const groupRef=useRef();
+  const groupRef = useRef();
   useFrame((state, delta) => {
     //1, we need to know hoe much time has passed sone the last frame
     //means delta time
     boxRef.current.rotation.y += delta;
-    groupRef.current.rotation.y+=delta;
+    groupRef.current.rotation.y += delta;
   });
   //2, like in Three.js, we can group objects with the Group class
   // Then are goinge to group th ecube and the sphere(not the floor) and them animate the group like a carousel
-
+  //3,letps make our scene more realistic and add some lights
+  //Add a directionalLight
+//i could go further and talk about other lights, enviromment maps, shadows, etc.
 
   return (
     <>
+
       <OrbitControls />
+      <ambientLight intensity={0.5}/>
+      <directionalLight intensity={1.5} position={[1, 2, 3]} />
       <group ref={groupRef}>
-      <mesh position-x={1.5} rotation-y={Math.PI * 0.25} scale={1} ref={boxRef}>
-        {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
-        <boxGeometry scale={1.5} />
-        <meshBasicMaterial args={[{ color: 'blue' }]} wireframe />
-      </mesh>
-      <mesh position-x={-1.5}>
-        <sphereGeometry />
-        <meshBasicMaterial color={'orange'} />
-      </mesh>
+        <mesh position-x={1.5} rotation-y={Math.PI * 0.25} scale={1} ref={boxRef}>
+          {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+          <boxGeometry scale={1.5} />
+          <meshStandardMaterial args={[{ color: 'blue' }]} wireframe={false} />
+        </mesh>
+        <mesh position-x={-1.5}>
+          <sphereGeometry />
+          <meshStandardMaterial color={'orange'} />
+        </mesh>
 
       </group>
-     
+
       <mesh scale={7} rotation-x={-Math.PI * 0.5} position-y={-1}>
         <planeGeometry />
-        <meshBasicMaterial color={'green'} />
+        <meshStandardMaterial color={'green'} />
       </mesh>
     </>
   );
